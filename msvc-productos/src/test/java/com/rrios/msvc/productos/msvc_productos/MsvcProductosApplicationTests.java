c0d1dff6f42c8dc734118b73b02958056461147c
Rafael <raf.rios@duocuc.cl>
2025-05-07 08:37:33 -0400

    Creacion de Microservicio "Productos"

---
+++ msvc-productos/src/test/java/com/rrios/msvc/productos/msvc_productos/MsvcProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrios.msvc.productos.msvc_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
