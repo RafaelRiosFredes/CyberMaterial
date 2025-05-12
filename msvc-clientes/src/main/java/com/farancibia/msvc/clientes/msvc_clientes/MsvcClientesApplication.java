11063f6a6484b6d72c46f945903faffdec06788a
Francisca <fr.arancibiac@duocuc.cl>
2025-05-12 09:12:00 -0400

    Creacion microservicios productos

---
+++ msvc-clientes/src/main/java/com/farancibia/msvc/clientes/msvc_clientes/MsvcClientesApplication.java
@@ -0,0 +1,13 @@
+package com.farancibia.msvc.clientes.msvc_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcClientesApplication.class, args);
+	}
+
+}
