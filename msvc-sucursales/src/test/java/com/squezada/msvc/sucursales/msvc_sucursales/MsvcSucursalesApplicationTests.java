ae7cf68e7677642115d0306e3a65a173b234ead3
squezada <savkita.quezada@gmail.com>
2025-05-13 09:02:40 -0400

    init commit

---
+++ msvc-sucursales/src/test/java/com/squezada/msvc/sucursales/msvc_sucursales/MsvcSucursalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squezada.msvc.sucursales.msvc_sucursales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcSucursalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
