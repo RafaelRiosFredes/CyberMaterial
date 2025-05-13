68b7ed5a754ecae1ed17d4cf70bcc04cb76446bd
Rafael <raf.rios@duocuc.cl>
2025-05-13 09:01:04 -0400

    Creacion de Microservicio "Boletas"

---
+++ msvc-boletas/src/main/java/com/rrios/msvc/boletas/msvc_boletas/MsvcBoletasApplication.java
@@ -0,0 +1,13 @@
+package com.rrios.msvc.boletas.msvc_boletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcBoletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcBoletasApplication.class, args);
+	}
+
+}
---
+++ msvc-boletas/src/test/java/com/rrios/msvc/boletas/msvc_boletas/MsvcBoletasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrios.msvc.boletas.msvc_boletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcBoletasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
