1661e3415c99c517245f1a347041d06cdc057917
squezada <savkita.quezada@gmail.com>
2025-05-24 15:10:55 -0400

    arreglo main

--- msvc-detallecompras/src/main/java/com/squezada/msvc/detallecompras/msvc_detallecompras/MsvcDetallecomprasApplication.java
+++ msvc-detallecompras/src/main/java/com/squezada/msvc/detallecompras/msvc_detallecompras/MsvcDetallecomprasApplication.java
@@ -2,7 +2,9 @@ package com.squezada.msvc.detallecompras.msvc_detallecompras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsvcDetallecomprasApplication {
 
