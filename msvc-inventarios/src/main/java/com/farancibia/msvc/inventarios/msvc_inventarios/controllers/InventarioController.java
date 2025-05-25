d120c02b8e02f5768f7c797a0cc021a297f68630
Francisca <fr.arancibiac@duocuc.cl>
2025-05-24 20:15:10 -0400

    modificacion en InventarioServiceImpl 2.1

--- msvc-inventarios/src/main/java/com/farancibia/msvc/inventarios/msvc_inventarios/controllers/InventarioController.java
+++ msvc-inventarios/src/main/java/com/farancibia/msvc/inventarios/msvc_inventarios/controllers/InventarioController.java
@@ -7,11 +7,13 @@ import com.farancibia.msvc.inventarios.msvc_inventarios.services.InventarioServi
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.print.attribute.HashAttributeSet;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/inventarios")
 public class InventarioController {
@@ -19,7 +21,7 @@ public class InventarioController {
     @Autowired
     private InventarioService inventarioService;
 
-    @GetMapping("/sucursal/{idSucursal}/producto/{idProducto}")
+    @GetMapping("/sucursales/{idSucursal}/productos/{idProducto}")
     public ResponseEntity<Inventario> getInventario(@PathVariable Long idSucursal, @PathVariable Long idProducto){
         return ResponseEntity.status(HttpStatus.OK).body(this.inventarioService.findByIdSucursalAndIdProducto(idSucursal, idProducto));
     }
