2bee7a50c8878f014dffb1538c8a11c993e031e0
Rafael <raf.rios@duocuc.cl>
2025-05-26 08:36:02 -0400

    cambio de puerto

--- msvc-productos/src/main/java/com/rrios/msvc/productos/msvc_productos/clients/InventarioClientRest.java
+++ msvc-productos/src/main/java/com/rrios/msvc/productos/msvc_productos/clients/InventarioClientRest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-inventarios",url="localhost:8082/api/v1/inventarios")
+@FeignClient(name="msvc-inventarios",url="localhost:8086/api/v1/inventarios")
 public interface InventarioClientRest {
     @GetMapping("/sucursal/{idSucursal}/producto/{idProducto")
     Inventario findByIdSucursalAndIdProducto(@PathVariable Long idSucursal,@PathVariable Long idProducto);
