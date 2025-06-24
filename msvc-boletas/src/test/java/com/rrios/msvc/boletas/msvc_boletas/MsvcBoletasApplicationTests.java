f59895aa92185a3cab7430c62a539df27c018633
Rafael <raf.rios@duocuc.cl>
2025-06-24 08:28:13 -0400

    Actualizacion de Inventario Test y Boleta

--- msvc-boletas/src/test/java/com/rrios/msvc/boletas/msvc_boletas/MsvcBoletasApplicationTests.java
+++ msvc-boletas/src/test/java/com/rrios/msvc/boletas/msvc_boletas/MsvcBoletasApplicationTests.java
@@ -2,7 +2,9 @@ package com.rrios.msvc.boletas.msvc_boletas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootTest
 class MsvcBoletasApplicationTests {
 
--- msvc-inventarios/src/main/java/com/farancibia/msvc/inventarios/msvc_inventarios/MsvcInventariosApplication.java
+++ msvc-inventarios/src/main/java/com/farancibia/msvc/inventarios/msvc_inventarios/MsvcInventariosApplication.java
@@ -2,10 +2,11 @@ package com.farancibia.msvc.inventarios.msvc_inventarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootTest
 public class MsvcInventariosApplication {
 
 	public static void main(String[] args) {
